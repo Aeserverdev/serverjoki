80106d240caeab7a4f3c46e7f82ed472b559f425
Aeserverdev <159782978+Aeserverdev@users.noreply.github.com>
2025-06-21 19:12:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -212,7 +212,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const box = document.getElementById("tabelRiwayat");
     box.textContent = "Memuat data...";
     try {
-      const res = await fetch(`https://script.google.com/macros/s/AKfycbzvm0RO0IdDk9dgowz7d56ZjOQUejBxjkiUzyOBaRAq5bbmQuLKoGa55sx_DCVW-ghd/exec?action=getRiwayat&npm=${user.npm}`);
+      const res = await fetch(`https://script.google.com/macros/s/AKfycbwH7KTXIGztdebIZJPH8PYW283DLLo7-hgmJthpeFFVSc4q3fEeliV18ec5IpJrl8lT/exec?action=getRiwayat&npm=${user.npm}`);
       const data = await res.json();
       semuaRiwayat = data;
       if (!Array.isArray(data) || data.length === 0) return box.innerHTML = "<i>Belum ada data.</i>";
@@ -253,7 +253,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const box = document.getElementById("tabelPembayaran");
     box.textContent = "Memuat data...";
     try {
-      const res = await fetch(`https://script.google.com/macros/s/AKfycbzvm0RO0IdDk9dgowz7d56ZjOQUejBxjkiUzyOBaRAq5bbmQuLKoGa55sx_DCVW-ghd/exec?action=getPembayaran&npm=${user.npm}`);
+      const res = await fetch(`https://script.google.com/macros/s/AKfycbwH7KTXIGztdebIZJPH8PYW283DLLo7-hgmJthpeFFVSc4q3fEeliV18ec5IpJrl8lT/exec?action=getPembayaran&npm=${user.npm}`);
       const data = await res.json();
       semuaPembayaran = data;
       if (!Array.isArray(data) || data.length === 0) return box.innerHTML = "<i>Belum ada data pembayaran.</i>";
